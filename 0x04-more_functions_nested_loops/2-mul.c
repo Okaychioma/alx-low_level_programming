98f4756be637532aa455e8e4111ad15cb651b577
Your Name <You@example.com>
2022-06-13 06:57:16 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * Mul - multiplies two integers
+ * mul - multiplies two given integers
  * @a: first given integer
  * @b:second given integer
- * Return - product of two integers
+ * Return - return product of two integers
  */
 
 int mul(int a, int b)
